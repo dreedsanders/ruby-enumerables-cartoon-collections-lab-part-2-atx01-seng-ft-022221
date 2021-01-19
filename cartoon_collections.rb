8221f876be98f50f3711330a8d6e2a82016a1160
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-19 23:23:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.include["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  planeteer_calls.include?["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
   #find an element in planeteer_calls that is also in valid_calls
   
